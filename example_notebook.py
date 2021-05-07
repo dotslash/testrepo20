ad2dc8643bbb3d04729462ec438425ec97d30332
pts@avah.dev <pts@avah.dev>
2021-05-07 16:53:57 +0000

    example notebook

---
+++ example_notebook.py
@@ -0,0 +1,20 @@
+# Databricks notebook source
+1+1
+
+# COMMAND ----------
+
+def myfunc1():
+  return "myfunc1"
+
+# COMMAND ----------
+
+myfunc1()
+
+# COMMAND ----------
+
+def myfunc1():
+  return "myfunc1v2"
+
+# COMMAND ----------
+
+myfunc1()
